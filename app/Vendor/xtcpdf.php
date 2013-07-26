261998bafa2a051ad3247a2098ae26dcd011360b
BettyM <betty.murillo@gorillanation.com>
2013-07-25 17:11:13 -0700

    tcpdf lib

---
+++ app/Vendor/xtcpdf.php
@@ -0,0 +1,48 @@
+<?php 
+App::import('Vendor','tcpdf/tcpdf'); 
+
+class XTCPDF  extends TCPDF 
+{ 
+    var $xheadertext  = 'Evolve Origin'; 
+    //var $xheadercolor = array(0,0,200); 
+	var $xheadercolor = array(40,40,40); 
+    var $xfootertext  = 'Evolve Media'; 
+    var $xfooterfont  = PDF_FONT_NAME_MAIN ; 
+    var $xfooterfontsize = 8 ; 
+
+
+    /** 
+    * Overwrites the default header 
+    * set the text in the view using 
+    *    $fpdf->xheadertext = 'YOUR ORGANIZATION'; 
+    * set the fill color in the view using 
+    *    $fpdf->xheadercolor = array(0,0,100); (r, g, b) 
+    * set the font in the view using 
+    *    $fpdf->setHeaderFont(array('YourFont','',fontsize)); 
+    */ 
+    function Header() 
+    { 
+		list($r, $b, $g) = $this->xheadercolor; 
+        $this->setY(10); // shouldn't be needed due to page margin, but helas, otherwise it's at the page top 
+        $this->SetFillColor($r, $b, $g); 
+        $this->SetTextColor(255 , 255, 255);
+		$this->Cell(0,10, '', 0, 1,'C', 1); 
+        $this->Text(15,13,$this->xheadertext ); 
+    } 
+
+    /** 
+    * Overwrites the default footer 
+    * set the text in the view using 
+    * $fpdf->xfootertext = 'Copyright © %d YOUR ORGANIZATION. All rights reserved.'; 
+    */ 
+    function Footer() 
+    { 
+        $year = date('Y'); 
+        $footertext = sprintf($this->xfootertext, $year); 
+        $this->SetY(-20); 
+        $this->SetTextColor(0, 0, 0); 
+        $this->SetFont($this->xfooterfont,'',$this->xfooterfontsize); 
+        $this->Cell(0,8, $footertext,'T',1,'C'); 
+    } 
+} 
+?>
